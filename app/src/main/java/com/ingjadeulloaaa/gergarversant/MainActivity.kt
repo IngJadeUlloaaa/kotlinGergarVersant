8d6dbc59ada8f2a75c1a9c1db0846eab722e43c0
Jade <ulloacarlos3112@gmail.com>
2023-11-26 22:06:20 -0600

    second configured

--- app/src/main/java/com/ingjadeulloaaa/gergarversant/MainActivity.kt
+++ app/src/main/java/com/ingjadeulloaaa/gergarversant/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ingjadeulloaaa.gergarversant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // btn welcome to navigate to menuActivity
+        val btnWelcome = findViewById<AppCompatButton>(R.id.btnWelcome)
+        btnWelcome.setOnClickListener {
+            navigateToMenuActivity()
+        }
+    }
+
+    // navigation
+    private fun navigateToMenuActivity() {
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
     }
 }
